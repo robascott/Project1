af256840e6d79f1995baaca04d0d9fbf4dabb7ff
Robin Scott <robinandrewscott@gmail.com>
2015-12-05 17:56:33 +0000

    Improved collision code

--- js/snake.js
+++ js/snake.js
@@ -2,18 +2,9 @@ $(document).ready(function() {
 	
 	var board = document.getElementById("board");
 
+	// Board dimensions
 	var rows = board.rows.length;
 	var columns = board.rows[0].cells.length;
-
-	
-	function checkTailCollision(body, cell) {
-	    for (var i = 0; i < body.length; i++) {
-	        if (body[i][0] == cell[0] && body[i][1] == cell[1]) {
-	            return true;
-	        }
-	    }
-	    return false;
-	}
 
 
 	function Snake(start) {
@@ -21,16 +12,16 @@ $(document).ready(function() {
 		this.currX = start[0];
 		this.currY = start[1];
 
-		this.snakeLength = 5;
+		this.snakeLength = 16;
+		this.snakeBody = [[this.currX,this.currY]]
+		this.moves = ["down"];
+		this.oldDirection = "down";
 
-		var oldDirection = "right";
+		for (i=this.snakeLength-2;i>=0;i--) {  // Build snake
+			this.snakeBody.push([5,i+5]);
+		}
 
-		this.moves = ["right"];
-
-		this.snakeBody = [[this.currX,this.currY],[9,10],[8,10],[7,10],[6,10]];
-
-
-		for (i=0; i<this.snakeLength; i++) {
+		for (i=0; i<this.snakeLength; i++) {  // Colour snake
 			var segment = this.snakeBody[i];
 			board.rows[segment[1]].cells[segment[0]].style.background = "black";
 		}
@@ -38,10 +29,11 @@ $(document).ready(function() {
 
 		var self = this;
 
+		// Keypress event listeners
 		$(window).keydown(function(e) {
 			currDir = null;
 			if (self.moves.length===0) {
-				currDir = oldDirection;
+				currDir = self.oldDirection;
 			}
 			if (e.keyCode===87) {  // W key
 				if (self.moves[0]!=="down" && self.moves[0]!=="up" && currDir!=="down") {
@@ -63,6 +55,7 @@ $(document).ready(function() {
 		});
 
 
+		// Move snake's position on board
 		this.updateSegments = function(X,Y) {
 			var currentPos = [X,Y].toString();
 			var foodPos = foodPosition.toString();
@@ -82,15 +75,17 @@ $(document).ready(function() {
 		}
 
 
+		// Return snake's next direction
 		this.getNextMove = function() {
 			if (this.moves.length===0) {
-				return oldDirection;
+				return this.oldDirection;
 			} else {
 				return this.moves.pop();
 			}
 		}
 
 
+		// Return next position of snake haid
 		this.getNextCell = function(dir) {
 			switch (dir) {
 				case "up":
@@ -106,18 +101,31 @@ $(document).ready(function() {
 					this.currX -= 1;
 			}
 			return [this.currX,this.currY];
-
 		}
 
 
+		// Check for collisions
 		this.isValid = function(cell) {
 			var valid = false;
-			if (cell[0]>=0&&cell[0]<columns&&cell[1]>=0&&cell[1]<rows && !checkTailCollision(this.snakeBody,cell)) {
+			if (cell[0]>=0&&cell[0]<columns&&cell[1]>=0&&cell[1]<rows && !this.checkTailCollision(cell)) {
 				return true;
 			}
 		}
 
+		
+		// Check if snake has collided with itself
+		this.checkTailCollision = function(cell) {
+			var body = this.snakeBody.slice(0, -1); 
+			for (var i = 0; i < body.length; i++) {
+				if (body[i][0] == cell[0] && body[i][1] == cell[1]) {
+					return true;
+				}
+			}
+			return false;
+		}
 
+
+		// Move head of snake
 		this.move = function() {
 			setTimeout(function(){
 
@@ -125,7 +133,7 @@ $(document).ready(function() {
 				var nextCell = self.getNextCell(newDirection);
 
 				if (self.isValid(nextCell)) {
-					oldDirection = newDirection;
+					self.oldDirection = newDirection;
 					
 					var nextX = nextCell[0];
 					var nextY = nextCell[1];
@@ -165,7 +173,7 @@ $(document).ready(function() {
 	}
 
 
-	var newGame = new Game([10,10]);
+	var newGame = new Game([5,20]);
 
 })
 
